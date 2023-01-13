0f81587be765be9138c2bafd90ae2c04bbf44dc0
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-01-13 16:04:38 +0700

    Lab14_1 finished

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum1 = 0,sum2 = 0,sum3 = 1,sum4 = 0,Max = A[0],Min = A[0]; 
+    for(int i=0;i<N;i++){
+        sum1 += A[i];
+    }
+    for(int i=0;i<N;i++){
+        sum2 += pow(A[i],2);
+    }
+    for(int i=0;i<N;i++){
+        sum3 *= A[i];
+    }
+    for(int i=0;i<N;i++){
+        sum4 += (1/A[i]);
+    }
+    for(int i=1;i<N;i++){
+        if(A[i]>Max) Max = A[i];
+        if(A[i]<Min) Min = A[i]; 
+    }
+    
+    B[0] = sum1/N;
+    B[1] = sqrt((sum2/N)-pow(sum1/N,2));
+    B[2] = pow(sum3,(1.0/N));
+    B[3] = (N/sum4);
+    B[4] = Max;
+    B[5] = Min;
+}
